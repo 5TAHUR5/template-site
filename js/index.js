d1cf703e4b9feefdaa196300c591c24cbaa73181
5TAHUR5 <salihovtahir2007@gmail.com>
2025-02-13 00:38:17 +0500

    Try adaptive 13

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const courseCards = document.querySelectorAll('.courseCard');
-let cardScrolling = false
+let cardScrollTimeout;
+let cardScroll = false;
+
 
 window.addEventListener('scroll', function() {
 
@@ -9,7 +11,10 @@ window.addEventListener('scroll', function() {
         // Проверяем, виден ли элемент в области просмотра
         if (!card.className.includes("visible") && (rect.top + ((this.window.screen.height/100)*20)) <= window.innerHeight && rect.bottom >= 0) {
             card.classList.add('visible');
-            cardScrolling = true
+            cardScrollTimeout = this.setTimeout(() => {
+                cardScroll = false
+            }, 1500)
+            cardScroll = true
             this.window.scrollTo({
                 top: rect.top + window.scrollY - ((this.window.screen.height/100)*3),
                 behavior: "smooth"
@@ -19,38 +24,40 @@ window.addEventListener('scroll', function() {
 });
 
 
-let maxScrollAmount = 100; // Максимальное количество пикселей для прокрутки
+let maxScrollAmount = 50; // Максимальное количество пикселей для прокрутки
 let isScrolling = false; // Флаг для отслеживания касания
 
-function limitScroll(event) {
-    if (!cardScrolling) {
-        event.preventDefault(); // Предотвращаем стандартное поведение прокрутки
+// function limitScroll(event) {
+//     event.preventDefault(); // Предотвращаем стандартное поведение прокрутки
 
-        // Определяем направление прокрутки
-        let delta = event.deltaY || event.touches[0].clientY; // Получаем значение прокрутки
-        let scrollDirection = delta > 0 ? Math.min(delta, maxScrollAmount) : Math.max(delta, -maxScrollAmount); // Ограничиваем прокрутку
+//     // Определяем направление прокрутки
+//     let delta = event.deltaY || event.touches[0].clientY; // Получаем значение прокрутки
+//     let scrollDirection = delta > 0 ? Math.min(delta, maxScrollAmount) : Math.max(delta, -maxScrollAmount); // Ограничиваем прокрутку
 
-        // Прокручиваем страницу вручную
-        window.scrollBy({
-            top: scrollDirection,
-            behavior: 'smooth' // Добавляем плавность прокрутки
-        });        
-    }
-}
+//     // Прокручиваем страницу вручную
+//     window.scrollBy({
+//         top: scrollDirection,
+//         behavior: 'smooth' // Добавляем плавность прокрутки
+//     });
+// }
 
 // Обработчики событий для мыши и сенсорных экранов
 // window.addEventListener('wheel', limitScroll, { passive: false });
 window.addEventListener('touchstart', function(event) {
     isScrolling = true; // Устанавливаем флаг при начале касания
-    cardScrolling = false
 });
 
 window.addEventListener('touchmove', function(event) {
-    if (isScrolling) {
-        limitScroll(event); // Вызываем функцию ограничения прокрутки
+    console.log("move", event)
+    // if (isScrolling) {
+    //     limitScroll(event); // Вызываем функцию ограничения прокрутки
+    // }
+    if (isScrolling && cardScroll) {
+        event.preventDefault();
     }
 }, { passive: false });
 
 window.addEventListener('touchend', function() {
     isScrolling = false; // Сбрасываем флаг при окончании касания
 });
+
