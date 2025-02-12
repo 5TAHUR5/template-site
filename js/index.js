defde5c292adf63d527395630dddac4d92e1011b
5TAHUR5 <salihovtahir2007@gmail.com>
2025-02-12 22:59:54 +0500

    Try adaptive 7

--- js/index.js
+++ js/index.js
@@ -8,12 +8,15 @@ window.addEventListener('scroll', function() {
         const rect = card.getBoundingClientRect();
 
         // Проверяем, виден ли элемент в области просмотра
-        if (!card.className.includes("visible") && Math.max((rect.top*1.8), 90) <= window.innerHeight && rect.bottom >= 0) {
+        if (!card.className.includes("visible") && rect.top*1.8 <= window.innerHeight && rect.bottom >= 0) {
             // Добавляем класс для анимации
-            card.scrollIntoView({ behavior: 'smooth' });
+            // card.scrollIntoView({});
             card.classList.add('visible');
-            
-
+            this.window.scrollTo({
+                top: rect.top + window.scrollY - (rect.height/10),
+                behavior: "smooth"
+            })
+            // this.window.scroll(0, rect.top)
         }
     });
 });
