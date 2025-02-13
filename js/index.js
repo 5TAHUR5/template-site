049d6339be011f57cf2cce5650ea3dce746b759a
5TAHUR5 <salihovtahir2007@gmail.com>
2025-02-13 15:32:32 +0500

    Try adaptive 1

--- js/index.js
+++ js/index.js
@@ -1,17 +1,21 @@
 const courseCards = document.querySelectorAll('.courseCard');
 
-
-
 window.addEventListener('scroll', function() {
 
     courseCards.forEach(card => {
         const rect = card.getBoundingClientRect();
 
-        // Проверяем, виден ли элемент в области просмотра
-        if (!card.className.includes("visible") && (rect.top + (rect.height*0.1)) <= window.innerHeight && rect.bottom >= 0) {
+        if (!card.className.includes("visible") && (rect.top + (rect.height*0.12)) <= window.innerHeight && rect.bottom >= 0) {
             card.classList.add('visible');
+            let toScrollY = 0
+            if (rect.height >= this.window.innerHeight) {
+                toScrollY = rect.top + window.scrollY - (rect.height*0.03)
+            } else {
+                toScrollY = rect.bottom + window.scrollY - window.innerHeight + (rect.height*0.1)
+            }
+
             this.window.scrollTo({
-                top: rect.bottom + window.scrollY - window.innerHeight + (rect.height*0.05),//rect.top + this.window.screen.height - rect.height + window.scrollY, //- ((rect.height/100)*5),
+                top: toScrollY,
                 behavior: "smooth"
             })
         }
